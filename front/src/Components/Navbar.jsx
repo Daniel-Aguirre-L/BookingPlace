aa9e9fee273f53b99f75befb965e62eea7f108cb
Jurany <adelerapi95@gmail.com>
2024-10-28 22:23:35 -0500

    Agregar Layout y agregar Navbar

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -1,7 +1,22 @@
 const Navbar = ()=> {
-    return (
-      <h1>Navbar</h1>
-    )
+
+  const onLogin = () => {
+    alert("Aqui va el login")
+  }
+
+  const onSignIn =() => {
+    alert("Aqui va el sign in")
+  }
+  return (
+    <div className="flex justify-between px-20 py-2.5">
+      <img src="/Icons/logoSvg.svg" alt="icono"/>
+      <div className="flex gap-5  ">
+        <button className="bg-[#088395] rounded-xl py-2 px-9 text-[#EEEEEEEE]" type="button"  onClick={onLogin}>Login</button>
+        <button className="bg-[#FBFFBD] rounded-xl py-2 px-9 text-[#112211]" type="button" onClick={onSignIn}>Sign in</button>
+      </div>
+    </div>
+  )
 }
+
 
 export default Navbar;
