bb75b5889eb3ae698282d1ac897d656fa9cc890a
mickslow <go.acosta3@gmail.com>
2024-10-30 00:53:14 -0300

    Add structure of DisplayCard component

--- front/src/Pages/Home.jsx
+++ front/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import Landing from "../Components/Landing";
 import Headline from "../Components/Headline";
 import CatalogList from "../Components/CatalogList";
+import DisplayCard from "../Components/DisplayCard";
 import { useEffect } from "react";
 import { rustikApi } from "../services/rustikApi";
 import { rustikEndpoints } from "../services/rustkEndPoints";
@@ -33,6 +34,7 @@ const Home = () => {
         tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
         sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
       </Headline>
+      <DisplayCard></DisplayCard>
     </>
   );
 };
