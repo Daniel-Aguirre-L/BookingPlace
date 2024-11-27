a3f1bab4087ba6d2366d10b696c9b775acf5ad98
nastacosta <go.acosta3@gmail.com>
2024-11-27 00:46:49 -0300

    Install react-booking-calendar, Add BookingCalendar component, Add EnterDate and LeaveDate to Landing SearchBar, Configure formatting of loaded dates, Create Styles for the calendar

---
+++ front/src/Components/BookingCalendar.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { Calendar } from "@demark-pro/react-booking-calendar";
+import useNotificationStore from "../store/useNotificationStore";
+import "../calendarVariables.css";
+
+const oneDay = 86400000;
+const today = new Date().getTime() + oneDay;
+
+const reserved = Array.from({ length: 3 }, (_, i) => {
+  const daysCount = Math.floor(Math.random() * (7 - 4) + 3);
+  const startDate = new Date(today + oneDay * 8 * i);
+
+  return {
+    startDate,
+    endDate: new Date(startDate.getTime() + oneDay * daysCount),
+  };
+});
+
+const weekDays = [0, 1, 2];
+
+function BookingCalendar({ setBookingDates, visible, setVisible }) {
+  const [selected, setSelected] = useState([]);
+  const { setNotification } = useNotificationStore();
+
+  const formatDate = (date) => {
+    const formattedDate = new Intl.DateTimeFormat("es-UY", {
+      weekday: "long",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }).format(date);
+
+    return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  };
+
+  useEffect(() => {
+    if (visible) {
+      if (selected[1]) {
+        const formattedDates = selected.map(formatDate);
+        setBookingDates(formattedDates);
+        setVisible(false);
+      }
+    }
+  }, [selected]);
+
+  return (
+    <Calendar
+      selected={selected}
+      reserved={reserved}
+      range={true}
+      protection={true}
+      options={{ locale: "es-UY", weekStartsOn: 1, useAttributes: true }}
+      onChange={setSelected}
+      className={`rounded-xl right-[10rem] top-[-8rem] transition-all duration-300 ease-in-out ${
+        visible
+          ? "opacity-100 scale-100 translate-y-0 visible"
+          : "opacity-0 scale-90 -translate-y-[-1rem] invisible"
+      }`}
+      disabled={(date, state) => {
+        return weekDays.includes(date.getDay());
+      }}
+    />
+  );
+}
+
+export default BookingCalendar;
--- front/src/Components/Landing.jsx
+++ front/src/Components/Landing.jsx
@@ -1,45 +1,76 @@
-
+import { useState } from "react";
+import BookingCalendar from "./BookingCalendar";
 
 const styles = {
-  background: 'linear-gradient(0deg, rgba(12, 17, 35, 1) 0%, rgba(0, 0, 0, 0) 30%), url("./img/landing.png")',
-  backgroundSize: 'cover'
+  background:
+    'linear-gradient(0deg, rgba(12, 17, 35, 1) 0%, rgba(0, 0, 0, 0) 30%), url("./img/landing.png")',
+  backgroundSize: "cover",
 };
 
 function Landing({ filter, setFilter, getNameCabins }) {
+  const [calendarVisible, setCalendarVisible] = useState(false)
+  const [bookingDates, setBookingDates] = useState(["", ""]);
+
+  const setDate = () => {
+    setCalendarVisible(true);
+  }
 
   const handleSearch = (e) => {
     e.preventDefault();
     getNameCabins(filter);
-    window.location.hash = 'cabañas';
+    window.location.hash = "cabañas";
   };
 
   return (
-    <section className="bg-background-dark max-w-[1600px] w-full max-md:mt-[-7rem] mt-[-6rem] mb-[7rem] h-[100vh] max-h-[60rem] flex flex-col justify-center montserrat overflow-hidden" style={styles} >
-      <h1 className="pageMargin mt-[8rem] text-primary-color font-bold text-[3.3rem] w-fit leading-[4rem] max-sm:text-[2.5rem] montserrat ">
+    <section
+      className="bg-background-dark max-w-[1600px] w-full max-md:mt-[-7rem] mt-[-6rem] mb-[7rem] h-[100vh] max-h-[60rem] flex flex-col justify-center montserrat overflow-hidden"
+      style={styles}
+    >
+      <h1 className="pageMargin mt-[6rem] text-primary-color font-bold text-[3.3rem] w-fit leading-[4rem] max-sm:text-[2.5rem] montserrat ">
         Escapa a la Naturaleza y <br />
         Reserva tu Cabaña ideal
       </h1>
       <p className="pageMargin mt-4 text-light-text text-[1.5rem] w-fit montserrat">
         Tu refugio natural, a un clic de distancia.
       </p>
-      <form 
-        className="pageMargin relative rounded-md bg-white flex text-[1.4rem] items-center mt-[3rem] h-[3.35rem] max-w-[50rem] overflow-hidden"
+      <form
+        className="pageMargin relative rounded-md bg-white flex text-[1.4rem] items-center mt-[3rem] h-[3.35rem] max-w-[50rem] font-montserrat"
         onSubmit={handleSearch}
       >
         <input
-          className="w-full bg-transparent ml-6 text-dark-text h-[50%] outline-none overflow-hidden"
+          className="w-full bg-transparent ml-6 text-dark-text h-[50%] outline-none overflow-hidden text-[1.2rem] "
           type="text"
-          placeholder="Buscar cabaña"
+          placeholder="Nombre de la cabaña"
           value={filter}
           onChange={(e) => setFilter(e.target.value)}
-          onFocus={()=>window.location.hash = ''}
+          onFocus={() => (window.location.hash = "")}
         />
-          <button
-            className="bg-primary-color flex justify-center items-center h-full w-[7rem]"
-            type="submit"
-          >
-            <img src="./Icons/search.svg" alt="buscar" />
-          </button>
+        <input
+          className="w-full bg-transparent ml-6 text-dark-text h-[50%] outline-none overflow-hidden text-[1.2rem] "
+          type="text"
+          placeholder="Fecha de Entrada"
+          value={bookingDates[0]}
+          onChange={(e) => setFilter(e.target.value)}
+          onClick={() => (setDate())}
+          readOnly
+        />
+        <input
+          className="w-full bg-transparent ml-5 text-dark-text h-[50%] outline-none overflow-hidden text-[1.2rem]"
+          type="text"
+          placeholder="Fecha de Salida"
+          value={bookingDates[1]}
+          onChange={(e) => setFilter(e.target.value)}
+          onClick={() => (setDate())}
+          readOnly
+        />
+
+        <button
+          className="bg-primary-color flex justify-center items-center h-full w-[14rem] rounded-e-md"
+          type="submit"
+        >
+          <img src="./Icons/search.svg" alt="buscar" width={22} />
+        </button>
+        <BookingCalendar setBookingDates={setBookingDates} visible={calendarVisible} setVisible={setCalendarVisible} />
       </form>
     </section>
   );
