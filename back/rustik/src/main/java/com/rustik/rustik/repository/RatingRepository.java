2de30fcad9707e6e38464d27aa2cfcfbd8db0fa1
Daniel-Aguirre-L <daniel_6.6@hotmail.com>
2024-11-27 21:28:19 -0500

    Correccion post rating

--- back/rustik/src/main/java/com/rustik/rustik/repository/RatingRepository.java
+++ back/rustik/src/main/java/com/rustik/rustik/repository/RatingRepository.java
@@ -9,11 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findByCabinId(Long cabinId);
     boolean existsByUserAndCabin(User user, Cabin cabin);
+    Optional<Rating> findByUserAndCabin(User user, Cabin cabin);
 
     @Query("SELECT AVG(r.score) as avgScore, COUNT(r.id) as totalRatings FROM Rating r WHERE r.cabin.id = :cabinId")
     List<Tuple> calculateAverageScoreAndCount(@Param("cabinId") Long cabinId);
