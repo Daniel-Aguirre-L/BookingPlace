581e0e3b99a4a7f56c7eec2a63e8be902488bbe1
JulianCallejas <juliecallejas@hotmail.com>
2024-11-30 19:48:24 -0500

    doble calendario

--- front/src/Components/SelectDate.jsx
+++ front/src/Components/SelectDate.jsx
@@ -6,8 +6,11 @@ import BookingCalendar from "./BookingCalendar";
 const SelectDate = ({ onClose, isOpen }) => {
   const { setNotification } = useNotificationStore();
   const [bookingDates, setBookingDates] = useState(["", ""]);
-  const calendarStyles = `scale-90 relative rounded-xl max-[500px]:scale-[0.75] md:right-auto transition-all duration-300 ease-in-out`;
-
+  const calendarStyles = [
+    `scale-[0.85] relative rounded-xl max-[500px]:scale-[0.90] md:right-auto transition-all duration-300 ease-in-out`,
+    `scale-[0.85] relative rounded-xl max-[500px]:scale-[0.90] md:right-auto transition-all duration-300 ease-in-out`
+  ];
+  
   const [feature, setFeature] = useState({
     id: null,
     name: "",
@@ -50,8 +53,8 @@ const SelectDate = ({ onClose, isOpen }) => {
   return (
     <div className="animate-fadeIn hs-overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 py-8 z-50 backdrop-blur">
       <div className="sm:max-w-lg sm:w-full m-3 sm:mx-auto">
-        <div className="flex flex-col items-center justify-center p-6">
-          <div className="w-full sm:w-full max-w-lg bg-white p-6 rounded-lg shadow-md relative z-10">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full sm:w-full max-w-lg bg-white py-6 rounded-lg shadow-md relative z-10">
             <div className="absolute top-0 left-0 right-0 bg-primary-color p-4 rounded-t-lg text-white z-20">
               <div className="flex items-center justify-between">
                 <h2 className="text-2xl font-light font-montserrat">
@@ -70,8 +73,8 @@ const SelectDate = ({ onClose, isOpen }) => {
               </div>
             </div>
 
-            <div className="pt-16">
-              <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="pt-14 md:pt-16 w-full">
+              <form onSubmit={handleSubmit} className="space-y-6 w-full">
                 <div className=" flex flex-col gap-6 justify-center items-center">
                   <BookingCalendar
                     setBookingDates={setBookingDates}
@@ -84,12 +87,12 @@ const SelectDate = ({ onClose, isOpen }) => {
                   <div className="flex flex-col gap-y-2">
                   {
                     bookingDates[0] &&
-                    <span className="flex justify-between items-center text-primary-color text-xs md:text-sm">
+                    <span className="flex justify-between items-center text-primary-color text-xs md:text-sm px-2">
                     <label htmlFor="bookingDates">Fecha de ingreso</label>
                     <label htmlFor="bookingDates">Fecha de salida</label>
                   </span>
                   }
-                  <span className="flex gap-3 md:gap-10 text-sm md:text-base">
+                  <span className="flex gap-3 md:gap-10 text-sm md:text-base px-2">
                     <InputField
                       id="bookingDates"
                       name="bookingDates"
@@ -115,7 +118,7 @@ const SelectDate = ({ onClose, isOpen }) => {
                     <p className="text-red-500 text-sm mt-1">{errors.name}</p>
                   )}
                 </div>
-                <div className="flex justify-center pb-5">
+                <div className="flex justify-center pb-5 px-5">
                   <button
                     type="submit"
                     className="w-full py-3 px-6 rounded-lg bg-primary-color text-background-white"
