87ddf53731f28409f413845dcdbc1c7892e9f63b
nastacosta <go.acosta3@gmail.com>
2024-12-09 21:51:27 -0300

    Modify LikeButton palette, Card brightness and Fix height of Comments component

--- front/src/Components/Card.jsx
+++ front/src/Components/Card.jsx
@@ -19,7 +19,7 @@ function Card({ title, children, price, images, id, favorites, refreshFavoritos 
       className="animate-fadeIn group relative card h-[32rem] rounded-[0.7rem] w-[98%] flex justify-end flex-col bg-cover bg-center bg-no-repeat transition-all hover:scale-105 "
       style={images && styles}
     >
-      <img src={images && images[0].url} alt="cabaña" className="w-full h-full absolute top-0 left-0 object-cover group-hover:opacity-0 group-hover:scale-0 transition-all duration-300 z-0 rounded-[0.7rem]" />
+      <img src={images && images[0].url} alt="cabaña" className="w-full brightness-[85%] h-full absolute top-0 left-0 object-cover group-hover:opacity-0 group-hover:scale-0 transition-all duration-300 z-0 rounded-[0.7rem]" />
       <section className="relative bg-[linear-gradient(0deg,rgba(12,17,35,0.95)_5%,rgba(0,0,0,0)_90%)] pb-5 pt-8 px-5">
         <div className="grid grid-cols-4 items-end gap-1" >
           <div className="col-span-3 flex flex-col justify-between">
